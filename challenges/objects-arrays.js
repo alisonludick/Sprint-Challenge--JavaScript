149fda3a09cb018746d7879189950c1354ec2f81
Alison Ludick <alison.ludick@gmail.com>
2019-08-23 11:08:25 +0200

    created new contactInfo array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -108,6 +108,12 @@ Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
 
+for (let i = 0; i < graduates.length; i++) {
+    contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
+
+console.log(`name and email address: ` + contactInfo);
+
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
